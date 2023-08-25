6576b7f5b54e46216d6939b7f6c278fc47e4aefb
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-08-25 14:54:07 +0200

    Add FileSize Validation

--- upload/validators.py
+++ upload/validators.py
@@ -1,8 +1,11 @@
 from django.core.exceptions import ValidationError
 
 def valid_file(value):
-    value=str(value)
-    if value.endswith('.pdf') != True and value.endswith('.txt') != True and value.endswith('.doc') != True:
-        raise ValidationError('Only PDF, DOC and TXT files are allowed')
+    if value.size > 10485760:
+        raise ValidationError('Only Files with maxsize 10Mb are allowed')
     else:
-        return value
+        value=str(value)
+        if value.endswith('.pdf') != True and value.endswith('.txt') != True and value.endswith('.doc') != True:
+            raise ValidationError('Only PDF, DOC and TXT files are allowed')
+        else:
+            return value
